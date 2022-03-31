5d60803328ad6a5764bb4aae3e7a63a313d0a50d
JamesH <jwhusc7@gmail.com>
2022-03-31 16:30:37 -0400

    testing tailwind properties and responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    colors: {
+      'blue': "#66B2FF",
+      'silver': '#ecebff',
+      'black': '#606070'
+    },
+    fontFamily: {
+      sans: ['Lato', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
