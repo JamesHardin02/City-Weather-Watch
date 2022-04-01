993032cedef0305d5609d193c3d87a33d34d40bb
JamesH <jwhusc7@gmail.com>
2022-04-01 09:59:24 -0400

    testing tailwind features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    colors: {
-      'blue': "#66B2FF",
-      'silver': '#ecebff',
-      'black': '#606070'
-    },
     fontFamily: {
       sans: ['Lato', 'sans-serif']
     },
