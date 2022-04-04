bc3198d8501efa4f7715f54b59e9a9b2a9f671e9
JamesH <jwhusc7@gmail.com>
2022-04-04 17:04:43 -0400

    working on five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,14 +34,18 @@ function displayWeather(cityName, currentWeather, wholeData) {
     fiveForecastDivEl.classList.add("flex");
     fiveForecastDivEl.classList.add("flex-col");
 
+    iconDateRowDivEl = document.createElement('div');
+    iconDateRowDivEl.classList.add('flex');
+
     let iconRowDivEl = document.createElement("div");
-    iconRowDivEl.classList.add("Flex");
+    iconRowDivEl.classList.add("flex");
+    let dateRowDivEl = document.createElement('div');
+    dateRowDivEl.classList.add('flex');
 
     let infoRowDivEl = document.createElement("div");
-    infoRowDivEl.classList.add("Flex")
+    infoRowDivEl.classList.add("flex");
 
-    fiveForecastDivEl.appendChild(iconRowDivEl)
-    fiveForecastDivEl.appendChild(infoRowDivEl);
+
     // end of five day forecast section 
 
     // current day description p elements
@@ -69,6 +73,36 @@ function displayWeather(cityName, currentWeather, wholeData) {
                 const windSpeed = wholeData[property]["wind_speed"];
                 pWindSpeedEl.textContent = "Wind speed: " + windSpeed + "mph"
                 break;
+
+            case "daily":
+                console.log(wholeData[property])
+                for (i=0; wholeData[property].length; i++){
+                    // icon getter
+                    const weatherData = wholeData[property];
+                    var icon = "http://openweathermap.org/img/wn/"+ weatherData["icon"] +"@2x.png"
+                    var iconImEl = document.createElement('img');
+                    iconImEl.setAttribute("alt", "Image of weather");
+                    iconImEl.setAttribute("src", icon);
+                    iconRowDivEl.appendChild(iconImEl);
+                    // date constructor
+                    const unixTimestamp = wholeData[property]["dt"];
+
+                    const milliseconds = unixTimestamp * 1000; // 1575909015000
+
+                    const dateObject = new Date(milliseconds);
+
+                    const day = dateObject.toLocaleString("en-US", {weekday: "long"}); // Monday
+                    const month = dateObject.toLocaleString("en-US", {month: "long"}); // December
+                    const dayNum = dateObject.toLocaleString("en-US", {day: "numeric"}); // 9
+                    const year = dateObject.toLocaleString("en-US", {year: "numeric"}); // 2022
+
+                    const date = day + ", " + month + " " + dayNum + ", " + year;
+                    let dateP = document.createElement("p");
+                    dateP.textContent = date;
+                    dateRowDivEl.appendChild(dateP);
+                    // end date constructor 
+                }
+                
         }
     }
 
@@ -94,8 +128,14 @@ function displayWeather(cityName, currentWeather, wholeData) {
     };
 
     // append all content to page
+    iconDateRowDivEl.appendChild(iconRowDivEl);
+    iconDateRowDivEl.appendChild(dateRowDivEl);
+    fiveForecastDivEl.appendChild(iconDateRowDivEl);
+    fiveForecastDivEl.appendChild(infoRowDivEl);
+
     headerDivEl.appendChild(h2DivEl);
     headerDivEl.appendChild(rightDivEl);
+
     currentWeatherDiv.appendChild(headerDivEl);
     currentWeatherDiv.appendChild(descDivEl);
     currentWeatherDiv.appendChild(fiveForecastDivEl);
