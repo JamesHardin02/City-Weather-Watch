53a26808a9144cf14bbb777737d7b79f2f8e3a61
JamesH <jwhusc7@gmail.com>
2022-04-04 14:49:04 -0400

    current day weather information displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,59 +6,99 @@ function displayWeather(cityName, currentWeather, wholeData) {
     while(currentWeatherDiv.firstChild){
         currentWeatherDiv.firstChild.remove();
     };
-
+    // city header
     let h2DivEl = document.createElement('div');
-    h2DivEl.classList.add("w-full")
-    h2DivEl.classList.add("basis-full")
+
     const cityH2El = document.createElement("h2")
     cityH2El.textContent = cityName;
     cityH2El.classList.add("text-lg")
     cityH2El.classList.add("text-center")
     h2DivEl.appendChild(cityH2El);
 
+    let rightDivEl = document.createElement('div');
+    // end of city header
+    
+    // current day description
+    let headerDivEl = document.createElement('div');
+    headerDivEl.classList.add("flex");
+    headerDivEl.classList.add("justify-center");
+
     let descDivEl = document.createElement('div');
     descDivEl.classList.add("flex");
-    descDivEl.classList.add("w-full");
-    descDivEl.classList.add("justify-around");
+    descDivEl.classList.add("flex-col");
+    descDivEl.classList.add("text-base");
+    // current day description
 
-    let leftDivEl = document.createElement('div');
-    leftDivEl.classList.add("flex");
-    leftDivEl.classList.add("flex-col");
+    // five day forecast section
+    let fiveForecastDivEl = document.createElement("div");
+    fiveForecastDivEl.classList.add("flex");
+    fiveForecastDivEl.classList.add("flex-col");
 
-    let rightDivEl = document.createElement('div');
+    let iconRowDivEl = document.createElement("div");
+    iconRowDivEl.classList.add("Flex");
 
-    // make control structure for wholeData instead of currentWeather
-    var pTempEl = document.createElement("p");
+    let infoRowDivEl = document.createElement("div");
+    infoRowDivEl.classList.add("Flex")
+
+    fiveForecastDivEl.appendChild(iconRowDivEl)
+    fiveForecastDivEl.appendChild(infoRowDivEl);
+    // end of five day forecast section 
+
+    // current day description p elements
+    let pTempEl = document.createElement("p");
+    let pFeelTempEl = document.createElement("p");
+    let pHumidityEl = document.createElement("p");
+    let pWindSpeedEl = document.createElement("p");
+
+    // construct loop
     for(const property in wholeData){
-        console.log(property)
-        if(property === "current"){
-            const kelvin = wholeData[property]["temp"];
-            let fahrenheit = Math.trunc((kelvin - 273.15) * 9/5 + 32)
-                pTempEl.textContent = "Temperature: "+ fahrenheit;
+        console.log(property);
+        switch(property){
+            case "current":
+                const kelvin = wholeData[property]["temp"];
+                let fahrenheit = Math.trunc((kelvin - 273.15) * 9/5 + 32)
+                pTempEl.textContent = "Temperature: "+ fahrenheit + "°";
+
+                const Fkelvin = wholeData[property]["feels_like"];
+                let Ffahrenheit = Math.trunc((Fkelvin - 273.15) * 9/5 + 32)
+                pFeelTempEl.textContent = "Feels like: "+ Ffahrenheit + "°";
+
+                const percentHumidity = wholeData[property]["humidity"];
+                pHumidityEl.textContent = "Humidity: " + percentHumidity + "%"
+
+                const windSpeed = wholeData[property]["wind_speed"];
+                pWindSpeedEl.textContent = "Wind speed: " + windSpeed + "mph"
+                break;
         }
     }
+
+    //append CURRENT weather loop
     for(const property in currentWeather){
         switch (property){
             case "description":
                 var pDescEl = document.createElement("p");
                 pDescEl.textContent = `${currentWeather[property]}`;
-                leftDivEl.appendChild(pDescEl);
-                leftDivEl.appendChild(pTempEl);
+                descDivEl.appendChild(pDescEl);
+                descDivEl.appendChild(pTempEl);
+                descDivEl.appendChild(pFeelTempEl);
+                descDivEl.appendChild(pHumidityEl);
+                descDivEl.appendChild(pWindSpeedEl);
                 break;
             case "icon":
                 var icon = "http://openweathermap.org/img/wn/"+ currentWeather[property] +"@2x.png"
                 var iconImEl = document.createElement('img');
                 iconImEl.setAttribute("alt", "Image of current weather")
                 iconImEl.setAttribute("src", icon)
-                iconImEl.classList.add("w-64")
-                iconImEl.classList.add("h-64")
                 rightDivEl.appendChild(iconImEl);
         };
     };
-    currentWeatherDiv.appendChild(h2DivEl);
-    descDivEl.appendChild(leftDivEl);
-    descDivEl.appendChild(rightDivEl);
+
+    // append all content to page
+    headerDivEl.appendChild(h2DivEl);
+    headerDivEl.appendChild(rightDivEl);
+    currentWeatherDiv.appendChild(headerDivEl);
     currentWeatherDiv.appendChild(descDivEl);
+    currentWeatherDiv.appendChild(fiveForecastDivEl);
 };
 
 function getWeather(initData, lat, lon){
