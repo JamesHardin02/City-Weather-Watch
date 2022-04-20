ad38b2a270d1101c7b35594aef64f7577052c733
JamesH <jwhusc7@gmail.com>
2022-04-20 13:15:33 -0400

    added comments to createWeatherInfoEl

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,63 +61,98 @@ function dateConstructor(unixTimestamp){
     dateP.textContent = date;
     return dateP;
 }
+/** <<** ----------- createWeatherInfoEl function ----------- **>>
+*  Description: 
+*  Creates weather descriptions/reports for either current weather or 5 day forecasts
 
+*  @param {object} data - an object containing data for either current day weather
+*                         or 5-day forecast
+*  @param {string} purpose - used in the switch expression to execute the code that uses the data parameter sent
+<<** ---------------------------------------------------------- **>> */
 function createWeatherInfoEl(data, purpose){
+    // loops through the data sent
     for(const property in data){
+        // checks which code block to execute
         switch(purpose){
+            // creates current weather report description elements
             case "current":
+                // main container which holds all description elements
                 let cElContainer = document.createElement('div');
                 cElContainer.classList.add('fxcol');
                 cElContainer.classList.add('default-text');
 
+                // date element
                 let date = dateConstructor(data['dt']);
+                // p element that introduces cpDescEl
                 let rightNowText = document.createElement('p');
                 rightNowText.classList.add('underline');
                 rightNowText.textContent = "Weather right now:";
+                // description of current weather
                 let cpDescEl = document.createElement('p');
                 cpDescEl.textContent = data["weather"][0]["description"];
+                // temperature right now
                 let pTempEl = document.createElement('p');
                 pTempEl.textContent = "Temperature: "+ kelvinToFahrenheit(data["temp"]) + "°";
+                // the temperature it feels like now
                 let pFeelTempEl = document.createElement("p");
                 pFeelTempEl.textContent = "Feels like: "+ kelvinToFahrenheit(data["feels_like"]) + "°";
+                // current humidity now
                 let pHumidityEl = document.createElement("p");
                 pHumidityEl.textContent = "Humidity: " + data["humidity"] + "%";
+                // wind speed in miles per hour right now
                 let pWindSpeedEl = document.createElement("p");
                 pWindSpeedEl.textContent = "Wind speed: " + data["wind_speed"] + " MPH";
+                // UV index number and safety color
                 let pUviIndex = uviWarningCode(data['uvi']);
 
+                // appends all description elements to cElContainer and returns it
                 cElContainer.append(date, rightNowText, cpDescEl, pTempEl, pFeelTempEl, pHumidityEl, pWindSpeedEl, pUviIndex);
-                //append CURRENT weather loop
                 return cElContainer
             break;
+            // creates 5-day forecast weather report description elements
             case "eightDay":
+                // main container which holds all description elements
                 let edElContainer = document.createElement('div');
                 edElContainer.classList.add('fxcol');
                 edElContainer.classList.add('default-text');
 
+                // temperature for the particular day's data that was sent
                 let pDayTempEl = document.createElement("p");
                 pDayTempEl.textContent = "Temp: " + kelvinToFahrenheit(data["temp"]['day']) + "°";
+                // Humidity for the particular day's data that was sent
                 let pDayHumidityEl = document.createElement("p");
                 pDayHumidityEl.textContent = "Humidity: " + data["humidity"] + "%";
+                // Wind speed for the particular day's data that was sent
                 let pDayWindSpeedEl = document.createElement('p');
                 pDayWindSpeedEl.textContent = "Wind: " + data["wind_speed"] + " MPH";
 
+                // appends the three weather data elements to the main container and returns it
                 edElContainer.append(pDayTempEl, pDayHumidityEl, pDayWindSpeedEl);
                 return edElContainer
             break;
         }
-        console.log(data);
     }
 }
 
+/** <<** ----------- createIconEl function ----------- **>>
+*  Description: 
+*  Create a weather symbol image from API's provided icon codes
+
+*  @param {number} iconCode - code is concatenated to the image src url
+*  @param {string} altDesc - the alt attribute content for the image tag
+*  @param {string} widthClass - (optional) A tailwind width class, 
+*                               purpose: responsiveness for the 5 day forecast icons.  
+<<** ---------------------------------------------------------- **>> */
 function createIconEl(iconCode, altDesc, widthClass){
+    // src url for iconImEl
     var icon = "http://openweathermap.org/img/wn/"+ iconCode +"@2x.png"
     var iconImEl = document.createElement('img');
     iconImEl.setAttribute("alt", altDesc);
     iconImEl.setAttribute("src", icon);
+    // optional responsive tailwind width class (currently only used for 5 day forecast)
     if(widthClass){
         iconImEl.classList.add(widthClass); // width at md breakpoint
-        iconImEl.classList.add("w-4/5") // width on small screens
+        iconImEl.classList.add("w-4/5") // default width on smallest screens
     }
     return iconImEl;
 };
